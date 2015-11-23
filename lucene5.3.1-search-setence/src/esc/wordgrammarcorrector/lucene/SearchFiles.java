7be5878260f8fe5e401e0ad234df78f41ebd8b24
Mility <zhangmingju1992@live.com>
2015-11-23 20:58:40 +0800

    First Version

---
+++ lucene5.3.1-search-setence/src/esc/wordgrammarcorrector/lucene/SearchFiles.java
@@ -0,0 +1,253 @@
+package esc.wordgrammarcorrector.lucene;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+/** Simple command-line based search demo. */
+public class SearchFiles {
+	
+	
+
+  SearchFiles() {}
+
+  /** Simple command-line based search demo. */
+  public static void main(String[] args) throws Exception {
+    String usage =
+      "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]\n\nSee http://lucene.apache.org/core/4_1_0/demo/ for details.";
+    if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+      System.out.println(usage);
+      System.exit(0);
+    }
+
+    String index = "resource/indexes";
+    String field = "sentence";
+    String queries = null;
+    int repeat = 0;
+    boolean raw = false;
+    String queryString = null;
+    int hitsPerPage = 10;
+    
+    for(int i = 0;i < args.length;i++) {
+      if ("-index".equals(args[i])) {
+        index = args[i+1];
+        i++;
+      } else if ("-field".equals(args[i])) {
+        field = args[i+1];
+        i++;
+      } else if ("-queries".equals(args[i])) {
+        queries = args[i+1];
+        i++;
+      } else if ("-query".equals(args[i])) {
+        queryString = args[i+1];
+        i++;
+      } else if ("-repeat".equals(args[i])) {
+        repeat = Integer.parseInt(args[i+1]);
+        i++;
+      } else if ("-raw".equals(args[i])) {
+        raw = true;
+      } else if ("-paging".equals(args[i])) {
+        hitsPerPage = Integer.parseInt(args[i+1]);
+        if (hitsPerPage <= 0) {
+          System.err.println("There must be at least 1 hit per page.");
+          System.exit(1);
+        }
+        i++;
+      }
+    }
+		/**
+		 * 1、WhitespaceAnalyzer
+		 * 
+		 * 仅仅是去除空格，对字符没有lowcase化,不支持中文；
+		 * 
+		 * 并且不对生成的词汇单元进行其他的规范化处理。
+		 * 
+		 * 2、SimpleAnalyzer
+		 * 
+		 * 功能强于WhitespaceAnalyzer,
+		 * 首先会通过非字母字符来分割文本信息，然后将词汇单元统一为小写形式。该分析器会去掉数字类型的字符。
+		 * 
+		 * 3、StopAnalyzer
+		 * 
+		 * StopAnalyzer的功能超越了SimpleAnalyzer，在SimpleAnalyzer的基础上增加了去除英文中的常用单词（
+		 * 如the，a等），也可以更加自己的需要设置常用单词；不支持中文
+		 * 
+		 * 4、StandardAnalyzer
+		 * 
+		 * 英文的处理能力同于StopAnalyzer.支持中文采用的方法为单字切分。他会将词汇单元转换成小写形式，并去除停用词和标点符号。
+		 */
+    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
+    IndexSearcher searcher = new IndexSearcher(reader);
+    //Analyzer analyzer = new StandardAnalyzer();//
+    //Analyzer analyzer = new WhitespaceAnalyzer();
+    Analyzer analyzer = new SimpleAnalyzer();
+
+    BufferedReader in = null;
+    if (queries != null) {
+      in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
+    } else {
+      in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    }
+    QueryParser parser = new QueryParser(field, analyzer);
+    while (true) {
+      if (queries == null && queryString == null) {                        // prompt the user
+        System.out.println("Enter query: ");
+      }
+
+      String line = queryString != null ? queryString : in.readLine();
+
+      if (line == null || line.length() == -1) {
+        break;
+      }
+
+      line = line.trim();
+      if (line.length() == 0) {
+        break;
+      }
+      
+      Query query = parser.parse(line);
+      System.out.println("Searching for: " + query.toString(field));
+            
+      if (repeat > 0) {                           // repeat & time as benchmark
+        Date start = new Date();
+        for (int i = 0; i < repeat; i++) {
+          searcher.search(query, 100);
+        }
+        Date end = new Date();
+        System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
+      }
+
+      doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
+
+      if (queryString != null) {
+        break;
+      }
+    }
+    reader.close();
+  }
+
+  /**
+   * This demonstrates a typical paging search scenario, where the search engine presents 
+   * pages of size n to the user. The user can then go to the next page if interested in
+   * the next hits.
+   * 
+   * When the query is executed for the first time, then only enough results are collected
+   * to fill 5 result pages. If the user wants to page beyond this limit, then the query
+   * is executed another time and all hits are collected.
+   * 
+   */
+  public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, 
+                                     int hitsPerPage, boolean raw, boolean interactive) throws IOException {
+ 
+    // Collect enough docs to show 5 pages
+    TopDocs results = searcher.search(query,  hitsPerPage);
+    //TopDocs results = searcher.search(query, 5 * hitsPerPage);
+    ScoreDoc[] hits = results.scoreDocs;
+    
+    int numTotalHits = results.totalHits;
+    System.out.println(numTotalHits + " total matching sentences");
+
+    int start = 0;
+    int end = Math.min(numTotalHits, hitsPerPage);
+        
+    while (true) {
+      if (end > hits.length) {
+        System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
+        System.out.println("Collect more (y/n) ?");
+        String line = in.readLine();
+        if (line.length() == 0 || line.charAt(0) == 'n') {
+          break;
+        }
+
+        hits = searcher.search(query, numTotalHits).scoreDocs;
+      }
+        int count = 1;
+        for (ScoreDoc hit : hits) {
+        	Document doc1 = searcher.doc(hit.doc);
+        	String res = doc1.get("sentence");
+        	//把句子的首字母变成大写 并把所有的i 替换成I 和所有的i'换成I'
+            String sentence = Character.toUpperCase(res.charAt(0))+res.substring(1, res.length()).replace("i ", "I ").replaceAll("i'", "I'");
+            System.err.println(count+"  "+sentence+", "+hit.score);
+            count++;
+        }
+      
+
+      if (!interactive || end == 0) {
+        break;
+      }
+
+      if (numTotalHits >= end) {
+        boolean quit = false;
+        while (true) {
+          System.out.print("Press ");
+          if (start - hitsPerPage >= 0) {
+            System.out.print("(p)revious page, ");  
+          }
+          if (start + hitsPerPage < numTotalHits) {
+            System.out.print("(n)ext page, ");
+          }
+          System.out.println("(q)uit or enter number to jump to a page.");
+          
+          String line = in.readLine();
+          if (line.length() == 0 || line.charAt(0)=='q') {
+            quit = true;
+            break;
+          }
+          if (line.charAt(0) == 'p') {
+            start = Math.max(0, start - hitsPerPage);
+            break;
+          } else if (line.charAt(0) == 'n') {
+            if (start + hitsPerPage < numTotalHits) {
+              start+=hitsPerPage;
+            }
+            break;
+          } else {
+            int page = Integer.parseInt(line);
+            if ((page - 1) * hitsPerPage < numTotalHits) {
+              start = (page - 1) * hitsPerPage;
+              break;
+            } else {
+              System.out.println("No such page");
+            }
+          }
+        }
+        if (quit) break;
+        end = Math.min(numTotalHits, start + hitsPerPage);
+      }
+    }
+  }
+}
+
